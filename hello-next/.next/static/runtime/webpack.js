264279f54a34631327b7afba168bf465ae7e69e8
venetiachou <venetiachou@gmail.com>
2018-11-15 12:48:02 +0800

    link api added

--- hello-next/.next/static/runtime/webpack.js
+++ hello-next/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fc4459db11c04b6a3ab4";
+/******/ 	var hotCurrentHash = "36d3d4d96bdaab3aa803";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
