5c359022db6777d66e0251d2ce8d5df2dd6736de
venetiachou <venetiachou@gmail.com>
2018-11-22 16:56:58 +0800

    finish two chapters while I got lots of errors in the later section

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../comps/MyLayout.js'
 
 const PostLink = (props) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>  
       <a>{props.title}</a>
     </Link>
   </li>
@@ -15,9 +15,9 @@ export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title="Hello Next.js"/>
-      <PostLink title="Learn Next.js is awesome"/>
-      <PostLink title="Deploy apps with Zeit"/>
+      <PostLink id="hello-nextjs" title="Hello Next.js"/>
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
     </ul>
   </Layout>
 )
